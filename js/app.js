508d2ce6e8e13e1e031926c84639627c7ad837ea
Nick-Hericks <nickhericks@gmail.com>
2019-01-25 19:32:46 +0200

    Tweet textarea event listener

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ const happening = document.querySelector('#happening');
 
 body.addEventListener('click', (event) => {
   if(event.target !== happening) {
-    happening.style.height = '80px';
+    happening.style.height = '38px';
   }
   else {
-    happening.style.height = '400px';
+    happening.style.height = '80px';
   }
 });
