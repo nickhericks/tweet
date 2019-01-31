bb922549dbbfca74097dade3120b87137acef99a
Nick-Hericks <nickhericks@gmail.com>
2019-01-31 18:12:03 +0200

    Happening toolbar hidden by default

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 
 const body = document.querySelector('body');
 const happening = document.querySelector('#happening');
+const happeningToolbar = document.querySelector('#happening-toolbar');
 
 
 
 body.addEventListener('click', (event) => {
   if(event.target !== happening) {
     happening.style.height = '38px';
+    happeningToolbar.style.display = "none";
   }
   else {
     happening.style.height = '80px';
+    happeningToolbar.style.display = "block";
   }
 });
