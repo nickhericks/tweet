66d5783bc378854d1c168037ff896f897070da2a
Nick-Hericks <nickhericks@gmail.com>
2019-02-07 15:23:32 +0200

    Initial tooltop

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const body = document.querySelector('body');
 const happening = document.querySelector('#happening');
 const happeningToolbar = document.querySelector('#happening-toolbar');
-
+const toolboxIcons = document.querySelectorAll('.toolbox-icon');
 
 
 body.addEventListener('click', (event) => {
@@ -14,4 +14,13 @@ body.addEventListener('click', (event) => {
     happening.style.height = '80px';
     happeningToolbar.style.display = "flex";
   }
+});
+
+toolboxIcons.forEach( function (icon) {
+  let span = document.createElement('span');
+  span.classList.add('tooltip');
+
+  span.textContent = 'hello';
+  // span.style.zIndex = '1000';
+  icon.parentNode.parentNode.prepend(span);
 });
